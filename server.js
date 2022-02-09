6cd9457d53fb5b3c1743b455db0b29edfc1feb29
Admin <shreeansh2m2000@gmail.com>
2022-02-09 20:56:56 +0530

    a

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const port = 8080
 app.get('/',(req,res)=>{
-    res.send("hiiiiiiiiiiiii")
+    res.send("hii")
 })
 
 app.listen(port,()=>{
